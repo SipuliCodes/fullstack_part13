8c76cc91725c89076548db42abc1e984cc1e2e81
SipuliCodes <vepsalainensisu@gmail.com>
2024-04-30 20:59:21 +0300

    13.17 done

--- models/index.js
+++ models/index.js
@@ -3,8 +3,6 @@ const User = require('./user')
 
 User.hasMany(Blog, {foreignKey: 'userId'})
 Blog.belongsTo(User, {foreignKey: 'userId'})
-Blog.sync({alter: true})
-User.sync({alter: true})
 
 module.exports = {
   Blog, User
