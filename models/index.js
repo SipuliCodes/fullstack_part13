0be1ebdebb861b25081711da87267d8458fc0293
SipuliCodes <vepsalainensisu@gmail.com>
2024-05-01 20:39:04 +0300

    13.19 done

--- models/index.js
+++ models/index.js
@@ -1,9 +1,13 @@
 const Blog = require('./blog')
 const User = require('./user')
+const Readinglist = require('./readinglist')
 
 User.hasMany(Blog, {foreignKey: 'userId'})
-Blog.belongsTo(User, {foreignKey: 'userId'})
+Blog.belongsTo(User, { foreignKey: 'userId' })
+
+User.belongsToMany(Blog, { through: Readinglist, as: 'readings' })
+Blog.belongsToMany(User, { through: Readinglist, as: 'users_readings' })
 
 module.exports = {
-  Blog, User
+  Blog, User, Readinglist
 }
